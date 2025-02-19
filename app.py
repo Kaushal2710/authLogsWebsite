6f2f60a2dfbc50464e48cfbcbda7977e3098ea40
Kaushal27 <kaushalambaliya69199@gmail.com>
2025-02-20 02:21:33 +0530

    Updated port

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from utils.logger import AuthLogger
 app = Flask(__name__)
 app.config.from_object(Config)
 
-PORT = int(os.getenv("PORT", 10000))  # Default to 10000 if PORT is not set
+PORT = int(os.getenv("PORT", 5000))  # Default to 10000 if PORT is not set
 app.run(host="0.0.0.0", port=PORT)
 
 # Initialize database
